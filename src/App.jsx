06dc4c59b28ffd5b3d2d4564442d8eaeaa8b312c
Santhosh13r <162345493+Santhosh13r@users.noreply.github.com>
2025-02-21 21:09:48 +0530

    B1 initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BillFrom from './Pages/BillFrom';
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <BillFrom/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css'
+
+const Navbar = () => {
+  return (
+   <>
+   
+   
+   
+   </>
+  )
+}
+
+export default Navbar
