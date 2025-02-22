bee6753ac898a732a898154fd63cb3861414eebf
Santhosh13r <162345493+Santhosh13r@users.noreply.github.com>
2025-02-22 09:26:49 +0530

    1st commit to delpoy stage

--- src/Pages/BillFrom.jsx
+++ src/Pages/BillFrom.jsx
@@ -3,7 +3,24 @@ import React from 'react'
 const BillFrom = () => {
   return (
     <>
-    
+    <header></header>
+    <section>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12 col-sm-3 col-md-12 mt-5 pt-2">
+            <div className="card shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+              <div className="card-body">
+                <div className="from">
+                     <form>
+                       <label htmlFor=""></label>
+                     </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     
     </>
   )
